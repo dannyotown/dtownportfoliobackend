a25100ce12405c29cf8a18cef88c30da53699ac0
Dtown <dannyotown@gmail.com>
2019-12-29 11:24:46 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const sgMail = require("@sendgrid/mail");
+const host = process.env.HOST || "0.0.0.0";
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+
+app.post("/api/email", checkBody(), async (req, res, next) => {
+  try {
+    sgMail.setApiKey(`${process.env.EMAILKEY}`);
+    const msg = {
+      to: "dannyotown@gmail.com",
+      from: `${req.body.email}`,
+      subject: `${req.body.subject}`,
+      text: `${req.body.body}`
+    };
+    await sgMail.send(msg);
+    res.status(202).json({ success: "Message sent successfully!" });
+  } catch (err) {
+    res.status(404).json({ error: "There was An Error Sending The Email" });
+  }
+});
+
+app.listen(port, host, () => {
+  console.log(`Running at http://${host}:${port}`);
+});
+
+function checkBody(req, res, next) {
+  return (req, res, next) => {
+    if (!req.body.email || !req.body.subject || !req.body.body) {
+      res.status(404).json({ error: "You are missing required fields" });
+    } else {
+      req = req;
+      next();
+    }
+  };
+}
